6d9c62dc107beaf83083f55bd1e153e5fec19a1a
creeperyang <yangcreeper@hotmail.com>
2015-12-18 22:59:38 +0800

    [update] add comment and remove unused code for image operation lib

--- src/util/image.js
+++ src/util/image.js
@@ -1,13 +1,17 @@
 import lwip from 'lwip';
 import Packer from './packer';
 
-const propArray = ['path', 'format', 'dimension', 'sizeMore', 'colorDepth', 'imageClass', 'size', 'time', 'time2'];
-const dimensionRegex = /(\d+)x(\d+)/;
+const transparent = [0, 0, 0, 0];
 
 const type = (operand) => {
     return Object.prototype.toString.call(operand).slice(8, -1).toLowerCase();
 };
-const transparent = [0, 0, 0, 0];
+
+/**
+ * open image
+ * @param  {String} filepath image path
+ * @return {Object}          promise
+ */
 const openImage = (filepath) => {
     return new Promise((resolve, reject) => {
         lwip.open(filepath, (err, image) => {
@@ -16,6 +20,13 @@ const openImage = (filepath) => {
     });
 };
 
+/**
+ * create a new blank image
+ * @param  {Number} width  width
+ * @param  {Number} height height
+ * @param  {Array|Object|String} color  bg color, default to transparent
+ * @return {Object}        promise
+ */
 const createImage = (width, height, color = transparent) => {
     return new Promise((resolve, reject) => {
         lwip.create(width, height, color, (err, image) => {
@@ -24,7 +35,17 @@ const createImage = (width, height, color = transparent) => {
     });
 };
 
-const appendImage = (batch, left, top, width, height, image) => {
+/**
+ * append source image to target image
+ * @param  {Object} batch  target image or target image's batch
+ * @param  {Object} image  source image
+ * @param  {Number} left   x coordinate
+ * @param  {Number} top    y coordinate
+ * @param  {Number} width  width
+ * @param  {Number} height height
+ * @return {Object}        target image
+ */
+const appendImage = (batch, image, left, top, width, height) => {
     for (let x = 0; x < width; x++) {
         for (let y = 0; y < height; y++) {
             batch.setPixel(left + x, top + y, image.getPixel(x, y));
@@ -33,6 +54,14 @@ const appendImage = (batch, left, top, width, height, image) => {
     return batch;
 };
 
+/**
+ * save image to file
+ * @param  {Object} image    image
+ * @param  {String} filepath file path
+ * @param  {String} format   image format: png|jpg|gif
+ * @param  {Object} params   format-specific parameters
+ * @return {Object}          promise
+ */
 const writeImage = (image, filepath, format, params) => {
     return new Promise((resolve, reject) => {
         image.writeFile(filepath, format, params, (err, image) => {
@@ -67,7 +96,7 @@ const mergeImage = async (sourceImgPaths, mergedImgPath, arrange = 'smart', marg
     let mergedImage = await createImage(pack.width, pack.height);
     let batch = mergedImage.batch();
     rects.reduce((preBatch, { pack: { x, y }, width, height, image}) => {
-        return appendImage(preBatch, x, y, width, height, image);
+        return appendImage(preBatch, image, x, y, width, height);
     }, batch);
     await writeImage(batch, mergedImgPath, 'png', {
         compression: 'high',
