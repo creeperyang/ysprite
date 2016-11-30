0b6439161140d9b5e92f8df7dd18d6eac3337ba2
creeperyang <yangcreeper@hotmail.com>
2016-11-30 18:17:47 +0800

    refactor lib/image and update test

--- test/merge-image.spec.js
+++ test/merge-image.spec.js
@@ -1,6 +1,7 @@
 const test = require('ava')
 const tempfile = require('tempfile')
 const mergeImage = require('../lib/image')
+const openImage = mergeImage.openImage
 
 test('mergeImage: should fail if sourceImgPaths or mergedImgPath invalid', t => {
     t.throws(mergeImage())
@@ -12,17 +13,32 @@ test('mergeImage: should fail if sourceImgPaths has nonexistent path', t => {
 })
 test('mergeImage: should merge image successfully', t => {
     const temp = tempfile('.png')
-    return mergeImage(['test/res/icons/app.png'], temp).then(info => {
+    const imgPath = 'test/res/icons/app.png'
+    return mergeImage([imgPath], temp).then(info => {
         t.deepEqual(info.source, [{
             x: 0,
             y: 0,
             margin: 0,
             width: 35,
             height: 35,
-            path: 'test/res/icons/app.png'
+            path: imgPath
         }])
-        t.is(info.merged.width, 35)
-        t.is(info.merged.height, 35)
-        t.is(info.merged.path, temp)
+        t.is(info.width, 35)
+        t.is(info.height, 35)
+        t.is(info.path, temp)
+        return openImage(imgPath).then(img => {
+            t.is(img.width(), 35)
+            t.is(img.height(), 35)
+            return new Promise((resolve, reject) => {
+                img.toBuffer('png', (err, buf1) => {
+                    t.ifError(err)
+                    info.image.toBuffer('png', (err, buf2) => {
+                        t.ifError(err)
+                        t.deepEqual(buf1, buf2)
+                        resolve()
+                    })
+                })
+            })
+        })
     })
 })
