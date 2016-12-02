b6080a3d0cf7cf81f8207f2b07ab28a629a81acb
creeperyang <yangcreeper@hotmail.com>
2016-12-02 17:21:46 +0800

    add ysprite func, add test

---
+++ test/index.spec.js
@@ -0,0 +1,35 @@
+const { basename } = require('path')
+const { readFileSync } = require('fs')
+const test = require('ava')
+const tempfile = require('tempfile')
+const ysprite = require('../lib')
+
+test('main entry#ysprite should work correctly', t => {
+    t.is(typeof ysprite.generateStyle, 'function')
+    t.is(typeof ysprite.generateSprite, 'function')
+    const imagePath = tempfile('.png')
+    const retinaImagePath = imagePath.replace(/\.png$/, '@2x.png')
+    const stylePath = tempfile('.css')
+    return ysprite('test/res/icons', {
+        sprite: {
+            retina: true,
+            imagePath,
+            retinaImagePath,
+            filter: '!**/*@2x.png',
+            margin: 0,
+            writeToFile: false
+        },
+        style: {
+            retina: true,
+            imagePath,
+            retinaImagePath,
+            stylePath,
+            banner: false,
+            writeToFile: false
+        }
+    }).then(res => {
+        t.ok(res.image instanceof Buffer)
+        t.ok(res.retina instanceof Buffer)
+        t.is(typeof res.style, 'string')
+    })
+})
